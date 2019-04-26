1fab3b47f283c31f3b5956a4c54fef17a63f7d6f
GausiVagos <simon.gauthier@condorcet.be>
2019-04-27 00:31:07 +0200

    (files generated)

---
+++ SmartBlasterAPI/SmartBlasterAPI/Controllers/MovementController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBlasterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovementController : ControllerBase
+    {
+        [HttpGet("TurnLeft")]
+        public async Task<IActionResult> Left()
+        {
+            
+
+            return Ok();
+        }
+
+        [HttpGet("TurnRight")]
+        public async Task<IActionResult> Right()
+        {
+
+
+            return Ok();
+        }
+
+        [HttpGet("GoForward")]
+        public async Task<IActionResult> MoveForward()
+        {
+
+
+            return Ok();
+        }
+
+        [HttpGet("GoBackward")]
+        public async Task<IActionResult> MoveBackward()
+        {
+
+
+            return Ok();
+        }
+    }
+}
