f6e6fc47b7e9a62ba8265257720834484d0860cd
Wilson Weets <wilson.weets@nsi-sa.be>
2019-04-27 16:25:29 +0200

    Api cam call

--- SmartBlasterUI/src/app/app.component.ts
+++ SmartBlasterUI/src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import ParticlesConfig from './../assets/data/particles.json';
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpRequest,
+  HttpHeaders,
+  HttpResponse
+} from '@angular/common/http';
 import { SignalRService } from './services/signal-r.service';
+import { Observable as __Observable } from 'rxjs';
+import { map as __map, filter as __filter } from 'rxjs/operators';
+import { StrictHttpResponse as __StrictHttpResponse } from '../strict-http-response';
 
 declare var particlesJS: any;
 
@@ -16,6 +24,7 @@ export class AppComponent implements OnInit {
       console.log('callback - particles.js config loaded');
     });
     this.signalRService.startConnection();
+    this.callCam().toPromise();
   }
 
   constructor(
@@ -28,9 +37,29 @@ export class AppComponent implements OnInit {
       ForwarBackward: FBDirection,
       RightLeft: RLDirection
     });
-  };
+  }
 
   public onOffClicked = () => {
     this.signalRService.broadcastStop();
-  };
+  }
+  // http://10.100.2.163:6500/api/Cam/snap
+  callCam() {
+    const __headers = new HttpHeaders();
+    const req = new HttpRequest<any>(
+      'POST',
+      `https://localhost:44399/api/Cam/snap`,
+      [],
+      {
+        headers: __headers,
+        responseType: 'json'
+      }
+    );
+
+    return this.http.request<any>(req).pipe(
+      __filter(_r => _r instanceof HttpResponse),
+      __map(_r => {
+        return _r as __StrictHttpResponse<{ image: Blob }>;
+      })
+    );
+  }
 }
