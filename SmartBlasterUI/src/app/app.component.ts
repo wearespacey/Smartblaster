5a0f732fe4f76d8ec93ff7c9bab32071d1456526
Denis Christopher <la171126@hotmail.com>
2019-04-27 02:40:45 +0200

    UI : Added main page

--- SmartBlasterUI/src/app/app.component.ts
+++ SmartBlasterUI/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import ParticlesConfig from './../assets/data/particles.json';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SmartBlasterUI';
+
+  ngOnInit() {
+    particlesJS('particles-js', ParticlesConfig, () => {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
