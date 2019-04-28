7379c4f384fe9413f77d53398a3549454af9e2aa
Wilson Weets <wilson.weets@nsi-sa.be>
2019-04-28 04:07:08 +0200

    Added custom vision calls

--- SmartBlasterUI/src/app/app.component.ts
+++ SmartBlasterUI/src/app/app.component.ts
@@ -10,9 +10,10 @@ import { SignalRService } from './services/signal-r.service';
 import { map as __map, filter as __filter } from 'rxjs/operators';
 import { StrictHttpResponse as __StrictHttpResponse } from '../strict-http-response';
 import { trigger, style, animate, transition } from '@angular/animations';
-import {Subject} from 'rxjs';
-import {WebcamImage} from 'ngx-webcam';
-import {CustomVisionPredictionService} from '@oneroomic/facecognitivelibrary';
+import { Subject } from 'rxjs';
+import { WebcamImage } from 'ngx-webcam';
+import { CustomVisionPredictionService } from '@oneroomic/facecognitivelibrary';
+import { API_KEYS } from 'src/api-keys.js';
 
 declare var particlesJS: any;
 
@@ -25,9 +26,10 @@ declare var particlesJS: any;
       transition('void => *', [
         // 'From' Style
         style({ transform: 'translateX(0%)' }),
-        animate('1500ms ease-in',
-            // 'To' Style
-            style({ transform: 'translateX(-100%)' }),
+        animate(
+          '1500ms ease-in',
+          // 'To' Style
+          style({ transform: 'translateX(-100%)' })
         )
       ])
     ]),
@@ -35,33 +37,44 @@ declare var particlesJS: any;
       transition('void => *', [
         // 'From' Style
         style({ transform: 'translateX(0%)' }),
-        animate('1500ms ease-in',
-            // 'To' Style
-            style({ transform: 'translateX(100%)' }),
+        animate(
+          '1500ms ease-in',
+          // 'To' Style
+          style({ transform: 'translateX(100%)' })
         )
       ])
-    ]),
+    ])
   ]
 })
 export class AppComponent implements OnInit {
   triggerSnapshot: Subject<void> = new Subject<void>();
   loaderMessage: any;
   isAnimating: boolean;
+  headers: HttpHeaders;
+  humanProbabilities: number[] = [];
+  isHuman: boolean;
+  interval: NodeJS.Timer;
 
   constructor(
     public signalRService: SignalRService,
     private http: HttpClient,
     private faceService: CustomVisionPredictionService
-  ) {}
+  ) {
+    this.headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+      'Access-Control-Allow-Headers': '*'
+    });
+  }
 
   ngOnInit() {
     this.loaderMessage = 'Détection de votre identité...';
     this.isAnimating = true;
     setTimeout(() => {
       this.isAnimating = false;
-      setInterval(() => {
+      this.interval = setInterval(() => {
         this.takeSnapshot();
-      }, 2000);
+      }, 1000);
     }, 1500);
     particlesJS('particles-js', ParticlesConfig, () => {
       console.log('callback - particles.js config loaded');
@@ -74,9 +87,44 @@ export class AppComponent implements OnInit {
   }
 
   async capturedImage(event: WebcamImage) {
-    //TODO  hardcoded keys
-    const predictions = await this.faceService.predictImage(this.b64toBlob(event.imageAsBase64), endpoint, key).toPromise();
-    console.log(predictions);
+    // TODO  hardcoded keys
+    const predictions: any = await this.predictImage(
+      this.b64toBlob(event.imageAsBase64),
+      API_KEYS['endpoint'],
+      API_KEYS['key'],
+      API_KEYS['projectId'],
+      API_KEYS['publishedName']
+    ).toPromise();
+    if (predictions) {
+      const humanPrediction: number = predictions.predictions.find(
+        p => p.tagName === 'humans'
+      ).probability;
+      if (this.humanProbabilities.length < 3) {
+        this.humanProbabilities.push(humanPrediction);
+      } else {
+        clearInterval(this.interval);
+        const globalHumanProb =
+          (this.humanProbabilities[0] +
+            this.humanProbabilities[1] +
+            this.humanProbabilities[2]) /
+          3;
+        this.isHuman = globalHumanProb >= 0.8;
+      }
+    }
+  }
+
+  predictImage = (stream, endpoint, key, projectId, publishedName) => {
+    let url;
+    let h = this.headers.set('Content-Type', 'application/octet-stream');
+    h = this.headers.set('Prediction-Key', key);
+    url =
+      endpoint +
+      '/' +
+      projectId +
+      '/classify/iterations/' +
+      publishedName +
+      '/image';
+    return this.http.post(url, stream, { headers: h });
   }
 
   b64toBlob(b64Data: string, contentType = '', sliceSize = 512) {
